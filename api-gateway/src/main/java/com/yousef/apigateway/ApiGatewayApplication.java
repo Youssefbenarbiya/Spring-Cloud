04afa556d58b0d1bd6c795d8e900dd1e9c497924
Youssefbenarbiya <benarbiayousef@nabeul.r-iset.tn>
2024-11-13 21:26:54 +0100

    update

--- api-gateway/src/main/java/com/yousef/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/yousef/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.codingworld.apigateway;
+package com.yousef.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
